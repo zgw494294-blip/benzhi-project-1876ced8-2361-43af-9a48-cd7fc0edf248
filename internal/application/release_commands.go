8dd87efc7ab7e0c950e1d2742bb7c4fdb65c0537
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 1876ced8-2361-43af-9a48-cd7fc0edf248-bug-2

--- internal/application/release_commands.go
+++ internal/application/release_commands.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"context"
+	"encoding/json"
 	"rigging-readiness-desk/internal/domain"
 )
 
@@ -9,10 +10,45 @@ func (s *Service) Freeze(ctx context.Context, id string, cmd VersionCommand) (*d
 	return s.mutation(ctx, id, cmd, "freeze", "MANIFEST_FROZEN", "冻结场景吊挂清单", func(session *domain.RiggingSession) error { _, err := session.BuildManifest(s.now()); return err })
 }
 func (s *Service) Issue(ctx context.Context, id string, cmd VersionCommand) (*domain.RiggingSession, error) {
-	return s.mutation(ctx, id, cmd, "issue", "CERTIFICATE_ISSUED", "签发吊挂启用凭据", func(session *domain.RiggingSession) error {
-		_, err := session.IssueCertificate(s.newID(), s.now())
-		return err
-	})
+	const operation = "issue"
+	unlock := s.locks.Lock(id)
+	defer unlock()
+	if cached, ok, err := s.idempotent(ctx, cmd.IdempotencyKey, operation); err != nil || ok {
+		return cached, err
+	}
+	session, err := s.repo.Get(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	if cmd.ExpectedVersion <= 0 {
+		return nil, domain.NewError(domain.ErrValidation, "expectedVersion", "必须大于零")
+	}
+	if session.Version != cmd.ExpectedVersion {
+		return nil, domain.NewError(domain.ErrConflict, "expectedVersion", "版本已变化，请刷新后重试")
+	}
+	// 幂等再签发：作业已凭据化且凭据有效时，返回原凭据，不递增版本、不重复写入规范化数据、不新增审计。
+	if session.Status == domain.StatusReleased && session.Certificate != nil {
+		return session, nil
+	}
+	if _, err := session.IssueCertificate(s.newID(), s.now()); err != nil {
+		return nil, err
+	}
+	expected := session.Version
+	session.Version++
+	session.UpdatedAt = s.now().UTC()
+	data, err := json.Marshal(session)
+	if err != nil {
+		return nil, err
+	}
+	event := AuditEvent{ID: s.newID(), SessionID: session.ID, Type: "CERTIFICATE_ISSUED", ActorID: cmd.ActorID, Detail: "签发吊挂启用凭据", CreatedAt: s.now().UTC()}
+	var idem *IdempotencyRecord
+	if cmd.IdempotencyKey != "" {
+		idem = &IdempotencyRecord{Key: cmd.IdempotencyKey, Operation: operation, SessionID: session.ID, Response: data, CreatedAt: s.now().UTC()}
+	}
+	if err := s.repo.Save(ctx, session, expected, event, idem); err != nil {
+		return nil, err
+	}
+	return session, nil
 }
 
 type VerificationResult struct {
